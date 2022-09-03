715d596ed39aaabde338367b06f9e88130ed2c29
Dominique Charlebois <domo.charlebois@gmail.com>
2022-09-03 11:09:15 -0700

    fix error with getMonth

--- app/routes/helpers/time.ts
+++ app/routes/helpers/time.ts
@@ -1,11 +1,12 @@
 export const getCurrentTerm = (): string => {
+  // Returns the month (0–11) in the specified date according to local time.
   const currentMonth = new Date().getMonth();
 
-  if (currentMonth >= 0 && currentMonth <= 4) {
+  if (currentMonth >= 0 && currentMonth <= 3) {
     return "Spring";
-  } else if (currentMonth >= 5 && currentMonth <= 08) {
+  } else if (currentMonth >= 4 && currentMonth <= 7) {
     return "Summer";
-  } else if (currentMonth >= 9 && currentMonth <= 12) {
+  } else if (currentMonth >= 8 && currentMonth <= 11) {
     return "Fall";
   } else {
     return "";
